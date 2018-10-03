b18a0834cac434d7fec7937ed8b4607ada1ae55a
Rob Brown <brown4homes@yahoo.com>
2018-10-03 11:14:39 -0700

    changed aws back

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,8 @@
+Aws.config.update({
+  region: 'us-east-1',
+  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
+})
+
+S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
 
 
-require 'aws-sdk-s3'
-          
-s3 = Aws::S3::Client.new(region: 'us-east-1')
-s3.create_bucket(bucket: 'reviews-eat-it')
